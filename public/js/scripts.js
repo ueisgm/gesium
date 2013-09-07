47756dba449ce9b3424462a66dc1567cf7d0b523
courlan <imgeusi@gmail.com>
2013-09-07 15:46:28 -0700

    switch from bootstrap3 to foundation

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,12 @@
 $(function() {
 	$("#image").change(function(){
-		var fileName = $(this).val();
-		fileName = fileName.substring(fileName.lastIndexOf("\\")+1);
-		var listItem = "<li class='list-group-item'>" + fileName + "</li>"
-		$("#fileName").append(listItem);
-		$("#submit_button").val("Start Upload")
+		// var fileName = $(this).val();
+		// fileName = fileName.substring(fileName.lastIndexOf("\\")+1);
+		// var listItem = "<li class='list-group-item'>" + fileName + "</li>"
+		// $("#fileName").append(listItem);
+		$("#upload_form").submit();
+		// $("#submit_button").val("Start Upload")
 	});
-
-	// $("#upload_btn").click(function() {
-	// 	$("#image").trigger("click");
-	// });
 
 	$("login_btn_container").mouseenter(function() {
 		$("login_text").css({opacity: 1});

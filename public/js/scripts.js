42c16f62fdde88fcebbce665af15f3d10e04320a
courlan <imgeusi@gmail.com>
2013-09-07 16:30:10 -0700

    restyle login page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,10 +5,14 @@ $(function() {
 		// var listItem = "<li class='list-group-item'>" + fileName + "</li>"
 		// $("#fileName").append(listItem);
 		$("#upload_form").submit();
-		// $("#submit_button").val("Start Upload")
 	});
 
 	$("login_btn_container").mouseenter(function() {
 		$("login_text").css({opacity: 1});
 	});
 });
+
+function submitForm(action) {
+    document.getElementById('login_form').action = action;
+    document.getElementById('login_form').submit();
+}
